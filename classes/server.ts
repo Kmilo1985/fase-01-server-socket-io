241f54502ddca952ccb9b6c062865e294b3f0289
kamilo <mckamilo@gmail.com>
2020-08-09 23:26:26 -0500

    Implementacion de socket, escuchar y obtener objeto desde el front

--- classes/server.ts
+++ classes/server.ts
@@ -1,18 +1,51 @@
-import express from 'express';
-import { SERVER_PORT } from '../global/environment';
+import express from "express";
+import { SERVER_PORT } from "../global/environment";
+import socketIO from "socket.io";
+import http from "http";
+import * as socket from "../sockets/socket";
+import { Socket } from "socket.io";
+import { mensaje } from "../sockets/socket";
 
 export default class Server {
+  private static _intance: Server;
+  public app: express.Application;
+  public port: number;
+  public io: socketIO.Server;
+  private httpServer: http.Server;
 
-    public app: express.Application;
-    public port:number
+  private constructor() {
+    this.app = express();
+    this.port = SERVER_PORT;
+    this.httpServer = new http.Server(this.app);
+    //  PARA UTILIZAR EXPRESS CON SOCKET IO SEGUIR IMPLEMENTACIÓN
+    this.io = socketIO(this.httpServer); //integro de socket io
+    this.escucharSockets();
+  }
 
-    constructor(){
-        this.app = express();
-        this.port = SERVER_PORT;
-    }
+  /**
+   *@description Secrea el patron singleton instanciando la clase constructor
+   * y asi derivando  la clase padre en la funcion get instance y generando una unica instancia
+   * @readonly
+   * @static
+   * @memberof Server
+   */
+  public static get instance() {
+    return this._intance || (this._intance = new this());
+  }
 
-    start( callback: any){
-    this.app.listen(this.port, callback );
-    }
+  private escucharSockets() {
+    console.log("escuchando sockets");
+  }
+  start(callback: any) {
+    this.httpServer.listen(this.port, callback);
+    this.io.on("connection", (cliente) => {
+      console.log("Nuevo cliente conectado");
 
+      // MENSAJE
+      socket.mensaje(cliente,this.io);
+
+      //   Desconectar
+      socket.desconectar(cliente);
+    });
+  }
 }
