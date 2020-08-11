f7847f84f1637174d9bfc6635127a05b962286a0
kamilo <mckamilo@gmail.com>
2020-08-10 23:37:26 -0500

    Proyecto inicial de chat creado
    Implementacion

--- sockets/socket.ts
+++ sockets/socket.ts
@@ -3,14 +3,14 @@ import socketIO from 'socket.io';
 
 export const desconectar = (cliente: Socket) => {
   cliente.on("disconnect", () => {
-    console.log("Cliente desconectado");
+    // console.log("Cliente desconectado");
   });
 };
 
 // ESCUCHAR MENSAJES
 export const mensaje = (cliente: Socket, io: socketIO.Server) => {
   cliente.on("mensaje", (payload: { de: string; cuerpo: string }) => {
-    console.log("mensaje recibido", payload);
+    // console.log("mensaje recibido", payload);
     io.emit('mensaje-nuevo', payload)
   });
 };
